5ac70a897f6caa0a5ee4ddf971d199c82a97ef99
mouna <maimouna93sow@gmail.com>
2017-01-26 12:49:12 +0100

    starting project

--- app/src/main/java/com/example/benzait27/raconte_moi/MainActivity.java
+++ app/src/main/java/com/example/benzait27/raconte_moi/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // starting new project
 
 
 
