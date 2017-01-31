198edb318059469e54f0e814d1cf105f38b2e719
mouna <maimouna93sow@gmail.com>
2017-01-31 16:02:16 +0100

    save

--- app/src/main/java/com/example/benzait27/raconte_moi/Inscription.java
+++ app/src/main/java/com/example/benzait27/raconte_moi/Inscription.java
@@ -2,21 +2,35 @@ package com.example.benzait27.raconte_moi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class Inscription extends AppCompatActivity {
+public class Inscription extends AppCompatActivity implements View.OnClickListener {
 
-    Button bSoumettre;
-    EditText nomUtilisateur, motDePasse;
+    Button bEnregister;
+    EditText etNom, etNbreEnfant, etNomUtilisateur, etMotDePasse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
 
-        nomUtilisateur = (EditText)findViewById(R.id.nomUtilisateur);
-        motDePasse = (EditText)findViewById(R.id.motDePasse);
-        bSoumettre = (Button)findViewById(R.id.bSoumettre);
+        etNom = (EditText)findViewById(R.id.etNom);
+        etNbreEnfant = (EditText)findViewById(R.id.etNbreEnfant);
+        etNomUtilisateur = (EditText)findViewById(R.id.etNomUtilisateur);
+        etMotDePasse = (EditText)findViewById(R.id.etMotDePasse);
+        bEnregister = (Button)findViewById(R.id.bEnregistre);
+
+        bEnregister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bEnregistre:
+
+                break;
+        }
     }
 }
