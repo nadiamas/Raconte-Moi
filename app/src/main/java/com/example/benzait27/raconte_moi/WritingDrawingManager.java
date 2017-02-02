1a86fb2b8d1f865f5d8e9f7f39027d87d93c9156
benzait27 <Zaki@BENZAIT1993>
2017-02-02 09:55:36 +0100

    add some design on the main interface

--- app/src/main/java/com/example/benzait27/raconte_moi/WritingDrawingManager.java
+++ app/src/main/java/com/example/benzait27/raconte_moi/WritingDrawingManager.java
@@ -9,5 +9,6 @@ public class WritingDrawingManager extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writing_drawing_manager);
+
     }
 }
