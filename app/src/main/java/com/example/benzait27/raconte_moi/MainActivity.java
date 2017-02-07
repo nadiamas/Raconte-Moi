cbc6984faa6e3ae59d4aa9c31997c04f2ae72c9e
benzait27 <Zaki@BENZAIT1993>
2017-02-07 13:59:18 +0100

    version 0.0.1 produit vide

--- app/src/main/java/com/example/benzait27/raconte_moi/MainActivity.java
+++ app/src/main/java/com/example/benzait27/raconte_moi/MainActivity.java
@@ -1,18 +1,13 @@
 package com.example.benzait27.raconte_moi;
 
 import android.app.Activity;
-import android.content.Intent;
+
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+
 
 public class MainActivity extends Activity implements View.OnClickListener {
-
-    Button bDeconnecter;
-    EditText etNom, etNbreEnfant, etNomUtilisateur;
 
 
     @Override
@@ -20,22 +15,13 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etNom = (EditText)findViewById(R.id.etNom);
-        etNbreEnfant = (EditText)findViewById(R.id.etNbreEnfant);
-        etNomUtilisateur = (EditText)findViewById(R.id.etNomUtilisateur);
-        bDeconnecter = (Button)findViewById(R.id.bDeconnecter);
 
-        bDeconnecter.setOnClickListener(this);
 
 
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.bDeconnecter:
-                //startActivity(new Intent(this, Authentification.class));
-                break;
-        }
+
     }
 }
